8bccacca16759c8fb135bde3845a6f4bf59bd7a1
Yuvaraja <yuvarajacpyr17@gmail.com>
2018-09-18 11:45:59 +0530

    Adding Main3Activity

---
+++ app/src/main/java/com/example/panneer/helloworldapplication/Main3Activity.java
@@ -0,0 +1,10 @@
+package com.example.panneer.helloworldapplication;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Yuva on 9/18/2018.
+ */
+
+public class Main3Activity extends Fragment {
+}
