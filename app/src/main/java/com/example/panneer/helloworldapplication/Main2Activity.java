160a07897b1f5d4c4ba259ccda22d0d825897857
Yuvaraja <yuvarajacpyr17@gmail.com>
2018-09-18 11:41:46 +0530

    Adding Main2Activity

---
+++ app/src/main/java/com/example/panneer/helloworldapplication/Main2Activity.java
@@ -0,0 +1,10 @@
+package com.example.panneer.helloworldapplication;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Yuva on 9/18/2018.
+ */
+
+public class Main2Activity extends Fragment{
+}
